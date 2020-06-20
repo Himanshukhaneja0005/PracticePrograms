764568ce4531393a78993499e58402e352dc5db0
Himanshukhaneja0005 <himkhane@WKWIN0492041.global.publicisgroupe.net>
2020-06-20 18:03:06 +0530

    HashMap Related Stuff

---
+++ DemoPrograms/src/com/hashmap/program/HashMapSize.java
@@ -0,0 +1,20 @@
+package com.hashmap.program;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapSize {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	 Map<String,Integer> hashmap= new HashMap<String,Integer>();
+	 hashmap.put("Hiamsnhu",12);
+	 hashmap.put("Khaneja", 15);
+	 int size=hashmap.size();
+	 int hash=hashmap.hashCode();
+	 System.out.println(hash);
+
+	}
+
+}
